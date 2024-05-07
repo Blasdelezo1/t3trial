a342eda3bb3d3b4d0cf636e1cfde00a852f23295
pedro roca de togores <pedrobgrdt@gmail.com>
2024-05-07 16:41:47 +0200

    db set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { db } from "~/server/db";
+
 
 
 const mockUrls = [
@@ -13,18 +15,29 @@ const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }));
-export default function HomePage() {
+export default async function HomePage() {
 
+  const posts = await db.query.posts.findMany()
+
+  console.log(posts)
 
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">{
-        [...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url} alt="image" />
-          </div>
-        ))
-      }
+      <div className="flex flex-wrap gap-4">
+        {
+          posts.map((post) => (
+            <div key={post.id}>{post.name}</div>
+          ))
+        }
+        {
+          [...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+            //just for getting errrors out, but dont use index as a key!!
+            <div key={image.id + '-' + index} className="w-48">
+
+              <img src={image.url} alt="image" />
+            </div>
+          ))
+        }
       </div>
     </main>
   );
