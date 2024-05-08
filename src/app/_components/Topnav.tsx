496c26ca94847f45e93bf6e18dcbc1afce976f56
pedro roca de togores <pedrobgrdt@gmail.com>
2024-05-08 18:56:32 +0200

    auth ready fixing module

---
+++ src/app/_components/Topnav.tsx
@@ -0,0 +1,20 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+export function TopNav() {
+    return (
+        <nav className="flex items-center justify-between w-full p-4 border-b text-xl font-semibold" >
+            <div>Gallery</div>
+
+
+            <div>
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
+
+        </nav>
+    )
+}
